5e7843947cf43ef9f74be4ccc9bbf0442886e751
Amibeia <aamibeiaa@gmail.com>
2024-06-02 18:37:18 +0700

    feat: setup Prettier, Eslint, and Shadcn UI components with custom CSS reset, add initial data of products and categories, add base model, and add utility functions for formatting number price to rupiah and lightening color

---
+++ src/lib/types.ts
@@ -0,0 +1,28 @@
+export type CategoryName =
+	| 'Flooring Materials'
+	| 'Fasteners and Hardware'
+	| 'Electrical Supplies'
+	| 'Concrete and Masonry'
+	| 'Painting Supplies'
+	| 'Plumbing Materials'
+	| 'Construction Tools'
+	| 'Adhesives'
+
+export type Category = {
+	id: string
+	name: CategoryName
+	color: string
+}
+
+export type Product = {
+	id: string
+	name: string
+	price: number
+	categoryId: Category['id']
+}
+
+export type RGB = {
+	r: number
+	g: number
+	b: number
+}
---
+++ src/lib/utils.ts
@@ -0,0 +1,85 @@
+import { clsx, type ClassValue } from 'clsx'
+import {
+	Bolt,
+	Brush,
+	Droplet,
+	Hammer,
+	HardHat,
+	Layers,
+	LucideIcon,
+	Package,
+	Zap,
+} from 'lucide-react'
+import { twMerge } from 'tailwind-merge'
+
+import { Category, RGB } from '@/lib/types'
+
+export function cn(...inputs: ClassValue[]) {
+	return twMerge(clsx(inputs))
+}
+
+export function rupiah(value: number) {
+	return new Intl.NumberFormat('id-ID', {
+		style: 'currency',
+		currency: 'IDR',
+	})
+		.format(value)
+		.slice(0, -3)
+}
+
+export function getCategoryIcon(category: Category['name']): LucideIcon {
+	switch (category) {
+		case 'Flooring Materials':
+			return Layers
+		case 'Fasteners and Hardware':
+			return Bolt
+		case 'Electrical Supplies':
+			return Zap
+		case 'Concrete and Masonry':
+			return HardHat
+		case 'Painting Supplies':
+			return Brush
+		case 'Plumbing Materials':
+			return Droplet
+		case 'Adhesives':
+			return Package
+		case 'Construction Tools':
+			return Hammer
+	}
+}
+
+function convert3To6DigitsHex(hex: string): string {
+	return (
+		'#' +
+		hex
+			.slice(1)
+			.split('')
+			.map((value) => value + value)
+			.join('')
+	)
+}
+
+function extractRgbFromHex(hex: string): RGB {
+	const removedHashHex = hex.slice(1)
+
+	const r = parseInt(removedHashHex.slice(0, 2), 16)
+	const g = parseInt(removedHashHex.slice(2, 4), 16)
+	const b = parseInt(removedHashHex.slice(4, 6), 16)
+
+	return { r, g, b }
+}
+
+function lightenHex(value: number, percent: number): number {
+	return Math.min(255, Math.floor(value + (255 - value) * (percent / 100)))
+}
+
+function formatHexValue(value: number): string {
+	return value.toString(16).padStart(2, '0')
+}
+
+export function lightenColor(hex: string, percent: number): string {
+	const formattedHex = hex.length === 3 ? convert3To6DigitsHex(hex) : hex
+	const { r, g, b } = extractRgbFromHex(formattedHex)
+
+	return `#${formatHexValue(lightenHex(r, percent))}${formatHexValue(lightenHex(g, percent))}${formatHexValue(lightenHex(b, percent))}`
+}
